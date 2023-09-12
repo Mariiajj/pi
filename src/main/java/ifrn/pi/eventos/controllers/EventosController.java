0a6dbc5cb0aab4913aa595c247f9aca60f720a28
Maria Baracho <maria.baracho@escolar.ifrn.edu.br>
2023-09-12 15:23:25 -0300

    Alterações realizadas na atividade 302

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -2,6 +2,8 @@ package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EventosController {
@@ -10,5 +12,17 @@ public class EventosController {
 	public String form() {
 		return "formEvento";
 	}
+
+	@RequestMapping(value = "/formsubimetido", method = RequestMethod.POST)
+	public String salvar(String nome, String local, String data, String horario) {
+		
+		System.out.println("Nome: " + nome);
+		System.out.println("Local: " + local);
+		System.out.println("Data: " + data);
+		System.out.println("Horário: " + horario);
+		
+		return "formConfirmacao";
+	}
+	
 
 }
